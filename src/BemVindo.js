68b8b4d7ea338fcd51110c420604c42712415228
Jonathan Gabriel da Silva Zanardo <endzs200@gmail.com>
2025-10-08 11:00:52 -0300

    Add files via upload

---
+++ src/BemVindo.js
@@ -0,0 +1,10 @@
+import Texto from "./Texto";
+export default function BemVindo() {
+    return (
+        <>
+            <h1>Olá Sala</h1>
+            <h2>Olá turma</h2>
+            <Texto/>
+        </>
+    );
+}
